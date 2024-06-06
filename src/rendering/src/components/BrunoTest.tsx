21a89fef3b60f0a0ec9783e5e477bc986a80132b
Brandon Bruno <brab@sitecore.net>
2024-06-06 16:04:20 -0400

    Added BrunoTest component.

---
+++ src/rendering/src/components/BrunoTest.tsx
@@ -0,0 +1,23 @@
+import { Text, Field, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type BrunoTestProps = ComponentProps & {
+  fields: {
+    Title: Field<string>;
+    Image: ImageField;
+    Body: Field<string>;
+  };
+};
+
+const BrunoTest = (props: BrunoTestProps): JSX.Element => {
+  const sxaStyles = `${props.params?.styles || ''}`;
+
+  return (
+    <section className={`sedemo-brunotest ${sxaStyles}`}>
+      <Text tag="h2" className="" field={props.fields.Title} />
+      <Text tag="p" className="" field={props.fields.Body} />
+    </section>
+  );
+};
+
+export const Default = withDatasourceCheck()<BrunoTestProps>(BrunoTest);
