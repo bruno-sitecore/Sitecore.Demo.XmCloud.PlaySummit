7f0ced3a5d201bbb20c57da5f8ace9fcf656c18b
Brandon Bruno <brab@sitecore.net>
2024-06-10 16:02:42 -0400

    Added SEDemo placeholder components.

---
+++ src/rendering/src/components/SEDemo/SEHeader.tsx
@@ -0,0 +1,30 @@
+import {
+  Text,
+  Field,
+  Image,
+  ImageField,
+  withDatasourceCheck,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type SEHeaderProps = ComponentProps & {
+  fields: {
+    Title: Field<string>;
+    Image: ImageField;
+    Body: Field<string>;
+  };
+};
+
+const SEHeader = (props: SEHeaderProps): JSX.Element => {
+  const sxaStyles = `${props.params?.styles || ''}`;
+
+  return (
+    <section className={`sedemo-brunotest ${sxaStyles}`}>
+      <Text tag="h2" className="" field={props.fields.Title} />
+      <Image tag="p" className="" field={props.fields.Image} />
+      <Text tag="p" className="" field={props.fields.Body} />
+    </section>
+  );
+};
+
+export const Default = withDatasourceCheck()<SEHeaderProps>(SEHeader);
