8b3104ce4ee01c2e834d09116f6924269934ceb8
Brandon Bruno <brab@sitecore.net>
2024-07-08 09:14:44 -0400

    Added header image, title rendering.

--- src/rendering/src/components/SEDemo/SEHeader.tsx
+++ src/rendering/src/components/SEDemo/SEHeader.tsx
@@ -12,18 +12,27 @@ type SEHeaderProps = ComponentProps & {
     Title: Field<string>;
     Image: ImageField;
     Body: Field<string>;
+    ShowTitleOnHeader: boolean;
   };
 };
 
 const SEHeader = (props: SEHeaderProps): JSX.Element => {
   const sxaStyles = `${props.params?.styles || ''}`;
 
+  const hasHeaderImage = props?.fields?.Image;
+  const showTitle = props?.fields?.ShowTitleOnHeader;
+
+  const headerImage = hasHeaderImage && <Image className="logo" field={props.fields.Image} />;
+  const title = showTitle && <h1>{props.fields.Title.value}</h1>;
+
   return (
-    <section className={`sedemo-brunotest ${sxaStyles}`}>
+    <header className={`${sxaStyles}`}>
+      {headerImage}
+      {title}
       <Text tag="h2" className="" field={props.fields.Title} />
-      <Image tag="p" className="" field={props.fields.Image} />
+      <Image className="" field={props.fields.Image} />
       <Text tag="p" className="" field={props.fields.Body} />
-    </section>
+    </header>
   );
 };
 
